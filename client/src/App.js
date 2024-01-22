87e8fdf1e879365912ac6766177aacf2f8b3462f
cnohilly <christopher.nohilly@snhu.edu>
2024-01-22 16:09:58 -0500

    Basic page layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@mui/material/Grid'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container direction='column' minHeight='100vh'>
+      <Grid item>
+HEader
+      </Grid>
+      <Grid item flexGrow='1'>
+Body
+      </Grid>
+      <Grid item>
+    Footer
+      </Grid>
+    </Grid>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
